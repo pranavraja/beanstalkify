6942d149c23b7f287d4d32c54c56753a9d09bdd6
Pranav Raja <rickdangerous1@gmail.com>
2013-08-05 21:09:06 +1000

    Remove useless tests

--- test/environment_spec.rb
+++
@@ -1,9 +0,0 @@
-require './environment'
-
-describe Environment do
-  environment = Environment.new("test")
-
-  it 'should get the status' do
-    #environment.status.should eq("Ready")
-  end
-end
